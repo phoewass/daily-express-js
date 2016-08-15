20e4e0daddcc601892b1164378554026f8115e4a
phoewass <phoewass@gmail.com>
2016-08-15 14:27:43 +0100

    add sqlite3 isntall in bootstrap scipt

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 #Provisionning
 wget -qO- https://deb.nodesource.com/setup_6.x | sudo bash - # This command includes apt-get update
-sudo apt-get -y install jq nodejs git g++# Nodejs 6.x includes npm
+sudo apt-get -y install sqlite3 jq nodejs git g++ # Nodejs 6.x includes npm
 sudo npm install nodemon gulp -g
 
 #Build
