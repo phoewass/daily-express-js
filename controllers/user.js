a33ca2cad5953fe90ea70748176e3e8680100af2
phoewass <phoewass@gmail.com>
2016-08-16 17:46:08 +0100

    fix create user function

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 exports.create = (req, res, next) => {
   const User = req.app.models.User;
 
-  User.Create({username:req.username, plainTextPassword: req.password});
+  User.create({username:req.username, plainTextPassword: req.password});
 };
